bb759e59f18636be1e16a810ff3daf568163a6aa
quyethv-03 <quyethoang312003@gmail.com>
2026-04-13 10:32:10 +0700

    feature: handle global exceptions

---
+++ src/main/java/org/example/commerce/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,72 @@
+package org.example.commerce.Exception;
+
+import org.example.commerce.DTO.Response.ErrorResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> validationErrors.put(error.getField(), error.getDefaultMessage()));
+        ErrorResponse errorRes = ErrorResponse.builder()
+                .code(status.value())
+                .status(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .errorCode("VALIDATION_FAILED")
+                .message("Invalid input data")
+                .data(validationErrors)
+                .build();
+        return ResponseEntity.status(status).body(errorRes);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorResponse error = ErrorResponse.builder()
+                .code(HttpStatus.BAD_REQUEST.value())
+                .status(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .errorCode("WRONG_FORMAT")
+                .message("Request body is missing or contains invalid JSON")
+                .data(null)
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorResponse error = ErrorResponse.builder()
+                .code(HttpStatus.METHOD_NOT_ALLOWED.value())
+                .status(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase())
+                .errorCode("METHOD_NOT_ALLOWED")
+                .message("HTTP method '" + ex.getMethod() + "' is not supported for this endpoint")
+                .data(null)
+                .build();
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(error);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+        ErrorResponse error = ErrorResponse.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .errorCode("UNCATEGORIZED_ERROR")
+                .message("An unexpected error occurred")
+                .data(null)
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+
+}
